25a5240be2a7281c315d735ae3de467287bf25f1
Felisa J Carbajal <fjcarbajal@ucdavis.edu>
2024-08-13 21:09:30 -0700

    Learning about strings

---
+++ session_05/strings.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+'''
+Python has the following data types built-in by default, in these categories:
+
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+'''
+
+# Examples of strings
+str1 = "Hello world"
+str2 = "Spring is coming!"
+str3 = "My dog ate 3 slices of my pizza"
+str4 = "I can't believe my dog did that"
