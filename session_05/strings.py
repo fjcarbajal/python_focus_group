68d89fd1cd459852a83a9d4f77fc8452dce079aa
Felisa J Carbajal <fjcarbajal@ucdavis.edu>
2024-08-14 21:12:24 -0700

    Properties and functions for string functions

--- session_05/strings.py
+++ session_05/strings.py
@@ -21,3 +21,34 @@ str4 = "I can't believe my dog did that"
 
 #You can overwrite functions
 #str() is a function so don't name anything as str = 
+
+# String operations
+original_str = "Viki is teaching me how to code in Python"
+
+upper_str = original_str.upper()
+print(upper_str)
+
+title_str = original_str.title()
+print(title_str)
+
+lower_str = original_str.lower()
+print(lower_str)
+
+replace_str = original_str.replace("Viki", "The internet")
+print(replace_str)
+
+# Stripping spaces
+spaced_out = " M y N a m e I s V i k i "
+right_strip = spaced_out.rstrip()
+left_strip = spaced_out.lstrip()
+
+print(right_strip)
+print(left_strip)
+
+#In Python, strings are considered objects
+# When dealing with objects, the syntax is function comes after the variable
+# Ex) upper_str.replace the function (replace) comes after the object
+# And since the functions we were learning (upper, lower) they are specific to strings
+# Then we follow object syntax and put the function afterwards
+
+# Google if you don't know if you are working with objects or other data types
