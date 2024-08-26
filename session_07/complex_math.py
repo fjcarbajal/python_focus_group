ba4fdae89f380c50020adb2b7b126c1d9f08009f
Felisa J Carbajal <fjcarbajal@ucdavis.edu>
2024-08-26 15:29:42 -0700

    Introducing math module

---
+++ session_07/complex_math.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import math
+
+'''
+This is the start of importing modules
+Modules are Python files that contain code that someone else has written
+This seems similar to installing R packages before runnning code
+'''
+
+#When working with a module and its function the syntax notation: module_name.function
+# Example) math.sqrt
+
+# Use square root for Pythagorean theorem
+a = 3
+b = 4
+
+hyp = math.sqrt(a**2 + b**2)
+print(hyp)
+
+# Use log2
+print(math.log2(0.25))
+
+# Use pi
+print(math.pi)
+
+# Use Euler's number
+print(math.e)
+
+# Use infinity 
+print(math.inf)
+
+# Use factorial
+print(math.factorial(a))
+
