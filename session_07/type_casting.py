4e88cbe06c5d222160f2caf464fee70c4f5a7b4e
Felisa J Carbajal <fjcarbajal@ucdavis.edu>
2024-08-25 22:17:24 -0700

    left off on Type Casting

---
+++ session_07/type_casting.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Assign numbers as an integer, float, and string
+a = 1
+b = 1.5
+c = '1'
+
+# Demonstrate type incompatibilities
+print(a + b)
+print("this did not give me an error?")
+
+print(a, type(a))
+print(b, type(b))
+
