35bc8d3cd429179044e1b148508c2a9f38ec8290
Felisa J Carbajal <fjcarbajal@ucdavis.edu>
2024-08-28 17:35:57 -0700

    Be mindful of how indexes and ranges work

---
+++ session_08/ranges.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Turn our range into a list
+numbers = list(range(1,6))
+print(numbers)
+
+# Turn our range into a list containing numbers 1-20
+range1_20 = list(range(1,21))
+print(range1_20)
+#Remember we have to add one - same formatting as with indexing
+
+# Even numbers from 1-10
+even_numbers=list(range(2,11,2)) #Follows (first number, last number +1, every xth value)
+print(even_numbers)
+odd_number = list(range(1,11,2))
+print(odd_number)
+other_number = list(range(0,11,2))
+print(odd_number)
+
