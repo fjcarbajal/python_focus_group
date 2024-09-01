6b427ac7660ca1c32af28789661343b6b5f64791
Felisa J Carbajal <fjcarbajal@ucdavis.edu>
2024-09-01 01:53:28 -0700

    setting up for loops

--- session_10/for_loops.py
+++ session_10/for_loops.py
@@ -49,4 +49,58 @@ for value in range(1,11): #your range of numbers 1-10 *remember index!!!**
 	squares.append(square) #add that squared value to the "squares" list
 print(squares) #prints the "squares" list
 
-print("I left off on Nested For-Loops")
+#Nesting means to put a for-loop inside a for-loop
+# Nested for-loop
+groupA = ["Janine", "Jessica", "Jules"]
+groupB = ["Osman", "Dag", "Logan"]
+
+for person1 in groupA:
+	for person2 in groupB:
+		print(person1, person2)
+        
+# By doing this, you can look at all combinations of variables
+# this is good when you have a dataset with nested metadata
+# Viki's Project Set-Up
+sexes = ["male", "female"]
+genotypes = ["WT", "MUT"]
+time_points = ["E18", "P30", "P60", "P120"]
+cell_types = ["Camk2a", "VIP"]
+
+print("All conditions for Viki's project:")
+for sex in sexes:
+	for genotype in genotypes:
+		for time_point in time_points:
+			for cell_type in cell_types:
+				print(sex, genotype, time_point, cell_type)
+
+# Parallel for-loop
+listA = [1, 2, 3, 4, 5]
+listB = ["A", "B", "C", "D", "E"]
+
+for A, B in zip(listA, listB):
+	print(f'Variable A is {A} and variable B is {B}. Here is AB: {A}{B}')
+
+# Rename variables
+for something1, something2 in zip(listA, listB):
+	print(something1, something2)
+
+# Switch the order of the lists
+for A, B in zip(listB, listA):
+	print(A, B)
+
+#This biggest thing to remember here is the order of how you zip() the two datasets
+
+
+ages = {"Viki": 24, "Logan": 25, "Osman": 30}
+
+# Access keys only
+for name in ages.keys():
+	print(name)
+
+# Access values only
+for age in ages.values():
+	print(age)
+
+# Access keys and values
+for name, age in ages.items():
+	print(f'{name} is {age} years old.')
