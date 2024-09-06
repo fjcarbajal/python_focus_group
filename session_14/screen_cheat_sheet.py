e3c0bb87b6d4b5e9cb4727551ea3752c50498275
Felisa J Carbajal <fjcarbajal@ucdavis.edu>
2024-09-05 22:24:21 -0700

    Screen Cheat Sheet

---
+++ session_14/screen_cheat_sheet.py
@@ -0,0 +1,8 @@
+'''
+Action & Command
+Start a screen session:	screen -S {name}
+Reattach to a screen session:	`screen -r {name}
+List your screens:	screen -ls
+Detach from a screen:	ctrl-a, d
+Terminate a screen:	ctrl-a, k
+Scroll through a screen:	ctrl-a, esc
