2959de7f87f6f64bfd69f87e686b8bb7c2507ca1
Felisa J Carbajal <fjcarbajal@ucdavis.edu>
2024-10-05 17:01:15 -0700

    S7 Extra Practice

---
+++ session_07/extra_practice.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+print("Problem 1:")
+var1 = "9.2"
+var2 = "17"
+var3 = "3.4"
+print(var1, type(var1))
+print(type(var2))
+print(type(var3))
+
+print("Problem 2:")
+#Converts var4 = "19" to an integer.
+var4 = "19"
+var4 = int(var4)
+print(var4, type(var4))
+
+print("Problem 3:")
+# Converts var5 = -26 to a string.
+
+'''
+
+
+Converts var6 = 83 to a float.
+Calculates the sum of var4 and var5.
+Calculates the difference between var2 and var1.
+Finds the product of var5 and var3.
+Raises var3 to the power of var1.
+Assign var7 = 9 and take the square root of it.
+Suppose you have a circle with a radius of 10 cm. What is the circumference of the circle? (Hint: Circumference = 2 * pi * radius)
+An employee documents their time driving to and from work since they get compensated for it. In the past week, they spent the following amount of time driving: 33, 37, 35, 31, 29, and 39 minutes. How many total hours is this?
+'''
